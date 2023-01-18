eb6deafb1aab611aadf091d7bfcaab87576b89e6
Bogdan <bogdangaponenko1996@gmail.com>
2023-01-18 00:08:37 -0600

    solve 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnCreate = document.querySelector("[data-create]");
+const btnClean = document.querySelector("[data-destroy]");
+const input = document.querySelector("input");
+const boxWrapper = document.getElementById("boxes");
+
+input.addEventListener("input", onInput);
+
+function onInput(event) {
+  let amount = 0;
+  amount = event.currentTarget.value;
+  btnCreate.addEventListener("click", createBox);
+
+  function createBox() {
+    for (let i = 0; i < amount; i += 1) {
+      let commonValue = 30 + i * 10;
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.style.cssText = `width: ${commonValue}px;
+       height: ${commonValue}px;
+        background-color: ${getRandomHexColor()};`;
+      boxWrapper.append(box);
+    }
+  }
+}
+
+btnClean.addEventListener("click", onCleanBtn);
+
+function onCleanBtn() {
+  boxWrapper.innerHTML = "";
+}
