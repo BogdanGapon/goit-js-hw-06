99e48455a313f07d088d23432651c8293037e325
Bogdan <bogdangaponenko1996@gmail.com>
2023-01-19 12:45:07 -0600

    add changes in 6 task (solve problem)

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,11 @@ const input = document.getElementById("validation-input");
 input.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
-  if (event.target.value.length !== Number(input.dataset.length)) {
-    input.classList.add("invalid");
-  } else {
-    input.classList.remove("invalid");
+  if (event.target.value.length === Number(input.dataset.length)) {
     input.classList.add("valid");
+    input.classList.remove("invalid");
+  } else {
+    input.classList.remove("valid");
+    input.classList.add("invalid");
   }
 }
